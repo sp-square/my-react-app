335b45f0081d570379ce67e028b5691bd3f3b2e7
Sandrine Poissonnet <spoisson@gmail.com>
2023-01-10 18:22:14 -0800

    update with gh-pages set-up

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const search = async (query) =>
-  axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${query}&rating=pg`);
+	axios.get(`https://www.omdbapi.com/?apikey=trilogy&t=${query}&rating=pg`);
 
 export default { search };
